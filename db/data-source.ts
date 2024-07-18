bf229deb542a75485c62102269942db79010b579
Maanas777 <maanasmohan77@gmail.com>
2024-07-18 22:11:44 +0530

    credentials removed

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,15 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Syamala@123',
-  database: 'ecommerce_api',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [],
   migrations: [],
   logging: false,
